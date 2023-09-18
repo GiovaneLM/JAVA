6edcdda6205a86084fadf39df0095e8d21a987e3
GiovaneLM <130477371+GiovaneLM@users.noreply.github.com>
2023-09-18 11:01:22 -0300

    Add files via upload

---
+++ aula0001e01.java
@@ -0,0 +1,11 @@
+/*
+1. Frase na tela - Implemente um programa que escreve na tela a frase 
+"O primeiro programa a gente nunca esquece!".
+*/
+public class aula0001e01
+{
+  public static void main(String[] args)
+  {
+    System.out.println("O primeiro projeto a gente nunca esquece!");
+  }
+}
---
+++ aula0001e02.java
@@ -0,0 +1,15 @@
+/*
+2. Etiqueta - Elabore um programa que escreve seu nome completo na primeira linha,
+seu endereço na segunda, e o CEP e telefone na terceira.
+*/
+public class aula0001e02
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Giovane Limeira Martins");
+        
+        System.out.println("Rua.Catulo da Paixão Cearense");
+
+        System.out.println("91750-260 | (51)994559463");
+    }
+}
