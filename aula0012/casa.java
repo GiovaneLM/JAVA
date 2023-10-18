3c080053a4383badf38d9005eab7ea8a0c0107be
GiovaneLM <130477371+GiovaneLM@users.noreply.github.com>
2023-10-18 11:38:31 -0300

    Add files via upload

---
+++ aula0012/casa.java
@@ -0,0 +1,29 @@
+package aula0012;
+
+public class casa extends imovel{
+    private float areaConst;
+
+    public casa(float valor, String endereco, float areaT, float areaConst) {
+        super(valor, endereco, areaT);
+        this.areaConst = areaConst;
+        System.out.println("tipo casa:");
+    }
+@Override
+    public void dados(){
+        super.dados();
+        System.out.println("area Construida:" + getAreaConst());
+        System.out.println("iptu:" + calculariptu());
+    }
+
+    public float calculariptu() {
+        return (getAreaConst() * 12) - ((getAreaConst() * 12) * 0.35f);
+    }
+
+    public float getAreaConst() {
+        return areaConst;
+    }
+    public void setAreaConst(float areaConst) {
+        this.areaConst = areaConst;
+    }
+    
+}
